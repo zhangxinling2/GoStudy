1dd1f4c1e45c6c2c33d88e04cf44170ec5947c8d
zhangxinling2 <2350486903@qq.com>
2023-02-10 09:23:07 +0800

    使用channel实现生产者消费者模型

---
+++ sync/produce-consume-channel_test.go
@@ -0,0 +1,50 @@
+﻿package sync
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+type Store struct {
+	init  sync.Once
+	max   int
+	store chan int
+}
+type cproducer struct{}
+type cconsumer struct{}
+
+func (s *Store) initStore() {
+	s.init.Do(func() {
+		s.store = make(chan int, s.max)
+	})
+}
+
+func (c cproducer) produce(s *Store) {
+	fmt.Println("开始生产，库存+1")
+	s.store <- 1
+}
+
+func (c cconsumer) consume(s *Store) {
+	fmt.Println("开始购买，库存-1")
+	<-s.store
+}
+func TestStore(t *testing.T) {
+	s := &Store{max: 10}
+	s.initStore()
+	cCount, pCount := 50, 50
+	for i := 0; i < pCount; i++ {
+		go func(i int) {
+			cproducer{}.produce(s)
+		}(i)
+	}
+	for i := 0; i < cCount; i++ {
+		go func(i int) {
+			cconsumer{}.consume(s)
+		}(i)
+	}
+	time.Sleep(5 * time.Second)
+	//fmt.Printf("库存余量:%d", s.count)
+	close(s.store)
+}
