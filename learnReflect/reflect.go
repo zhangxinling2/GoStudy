f50da9ef79be7b4793b6b5487dda77656a6717d3
zhangxinling2 <2350486903@qq.com>
2023-03-21 08:00:41 +0800

    反射学习

--- learnReflect/reflect.go
+++ learnReflect/reflect.go
@@ -6,6 +6,23 @@ import (
 	"reflect"
 )
 
+func SetColumn(entity any, field string, newVal any) error {
+	if entity == nil {
+		return errors.New("不支持 nil")
+	}
+	typ := reflect.TypeOf(entity)
+	if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
+		return errors.New("非法类型")
+	}
+	vals := reflect.ValueOf(entity)
+	vals = vals.Elem()
+	val := vals.FieldByName(field)
+	if !val.CanSet() {
+		return errors.New(fmt.Sprintf("%s不能被设置", field))
+	}
+	val.Set(reflect.ValueOf(newVal))
+	return nil
+}
 func structReflect(entity any) (map[string]any, error) {
 	if entity == nil {
 		return nil, errors.New("不支持 nil")
@@ -38,4 +55,79 @@ func structReflect(entity any) (map[string]any, error) {
 		}
 	}
 	return res, nil
+}
+
+func IterateFunc(entity any) (map[string]FuncInfo, error) {
+	typ := reflect.TypeOf(entity)                                  //得到类型信息
+	if typ.Kind() != reflect.Ptr && typ.Kind() != reflect.Struct { //判断是否为结构体或指针
+		return nil, errors.New("非法类型")
+	}
+	numFunc := typ.NumMethod() //得到方法数量
+	result := make(map[string]FuncInfo, numFunc)
+	for i := 0; i < numFunc; i++ {
+
+		m := typ.Method(i)                                       //typ.Method(i)得到Method
+		num := m.Type.NumIn()                                    //.Type得到方法信息 .NumIn()得到输入数量
+		fn := m.Func                                             //.Func是方法的Value
+		input := make([]reflect.Type, 0, num)                    //input是输入参数的类型
+		inputValue := make([]reflect.Value, 0, num)              //inputValue是输入参数的值
+		inputValue = append(inputValue, reflect.ValueOf(entity)) //输入的第一个永远是结构体本身，就如同java的this
+		for j := 0; j < num; j++ {
+
+			fnInType := fn.Type().In(j) //In返回的是第j个参数的类型
+			input = append(input, fnInType)
+			if j > 0 {
+				inputValue = append(inputValue, reflect.Zero(fnInType)) //输入都用0值即可，用来测试
+			}
+		}
+		outNum := m.Type.NumOut()
+		output := make([]reflect.Type, 0, outNum)
+		for j := 0; j < outNum; j++ {
+			output = append(output, fn.Type().Out(j))
+		}
+
+		resValues := fn.Call(inputValue)
+		results := make([]any, 0, len(resValues))
+		for _, v := range resValues {
+			results = append(results, v.Interface())
+		}
+		funcInfo := FuncInfo{
+			Name:   m.Name,
+			Input:  input,
+			Output: output,
+			Result: results,
+		}
+		result[m.Name] = funcInfo
+	}
+	return result, nil
+}
+
+type FuncInfo struct {
+	Name   string
+	Input  []reflect.Type
+	Output []reflect.Type
+	Result []any
+}
+
+func IterateSlice(entity any) error {
+	typ := reflect.TypeOf(entity)
+	val := reflect.ValueOf(entity)
+
+	switch typ.Kind() {
+	case reflect.Slice:
+		res := make([]any, 0, val.Len())
+		for i := 0; i < val.Len(); i++ {
+			res = append(res, val.Index(i).Interface())
+		}
+		return nil
+	case reflect.Map:
+		key := make([]any, 0, val.Len())
+		value := make([]any, 0, val.Len())
+		for _, k := range val.MapKeys() {
+			key = append(key, k.Interface())
+			value = append(value, k.MapIndex(k).Interface())
+		}
+		return nil
+	}
+	return nil
 }
--- learnReflect/reflect_test.go
+++ learnReflect/reflect_test.go
@@ -2,15 +2,34 @@ package learnReflect
 
 import (
 	"errors"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
+type User struct {
+	Name string
+	age  int
+}
+
+func NewUser(name string, age int) User {
+	return User{Name: name, age: age}
+}
+func NewUserPtr(name string, age int) *User {
+	return &User{Name: name, age: age}
+}
+func (u User) GetName() string {
+	return u.Name
+}
+func (u *User) GetAge() int {
+	return u.age
+}
+func (u *User) ChangeName(name string) {
+	u.Name = name
+}
 func Test_structReflect(t *testing.T) {
-	type User struct {
-		Name string
-		age  int
-	}
+
 	testCases := []struct {
 		name    string
 		entity  any
@@ -39,6 +58,100 @@ func Test_structReflect(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			res, err := structReflect(tc.entity)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantRes, res)
+		})
+	}
+}
+
+func TestSetColumn(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		entity     any
+		field      string
+		newVal     any
+		wantErr    error
+		wantEntity any
+	}{
+		{
+			name:       "CanSet",
+			entity:     &User{Name: "Tom", age: 18},
+			field:      "Name",
+			newVal:     "Jerry",
+			wantEntity: &User{Name: "Jerry", age: 18},
+		},
+		{
+			name:    "Can not Set",
+			entity:  &User{Name: "Tom", age: 18},
+			field:   "age",
+			newVal:  19,
+			wantErr: errors.New(fmt.Sprintf("age不能被设置")),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := SetColumn(tc.entity, tc.field, tc.newVal)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantEntity, tc.entity)
+		})
+	}
+}
+
+func TestIterateFunc(t *testing.T) {
+	tests := []struct {
+		name    string
+		entity  any
+		args    []any
+		wantRes map[string]FuncInfo
+		wantErr error
+	}{
+		{
+			name:   "struct",
+			entity: NewUser("Tom", 18),
+			wantRes: map[string]FuncInfo{
+				"GetName": {
+					Name:   "GetName",
+					Input:  []reflect.Type{reflect.TypeOf(User{})},
+					Output: []reflect.Type{reflect.TypeOf("")},
+					Result: []any{"Tom"},
+				},
+			},
+		},
+		{
+			name:   "point",
+			entity: NewUserPtr("Tom", 18),
+			wantRes: map[string]FuncInfo{
+				"ChangeName": {
+					Name:   "ChangeName",
+					Input:  []reflect.Type{reflect.TypeOf(&User{}), reflect.TypeOf("")},
+					Output: []reflect.Type{},
+					Result: []any{},
+				},
+				"GetAge": {
+					Name:   "GetAge",
+					Input:  []reflect.Type{reflect.TypeOf(&User{})},
+					Output: []reflect.Type{reflect.TypeOf(0)},
+					Result: []any{18},
+				},
+				"GetName": {
+					Name:   "GetName",
+					Input:  []reflect.Type{reflect.TypeOf(&User{})},
+					Output: []reflect.Type{reflect.TypeOf("")},
+					Result: []any{""},
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := IterateFunc(tc.entity)
 			assert.Equal(t, tc.wantErr, err)
 			if err != nil {
 				return
