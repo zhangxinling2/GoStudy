59d90bb947e91e0da933e5ab671a162a0a194a41
zhangxinling2 <2350486903@qq.com>
2023-03-22 08:07:14 +0800

    集成式的error

---
+++ internal/errs/error.go
@@ -0,0 +1,22 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+)
+
+//一般不带参数直接声明
+
+var (
+	ErrPointerOnly = errors.New("orm:只支持一级指针作为输入")
+)
+
+//带参数的可声明为函数
+
+func NewErrUnsupportedExpression(expr any) error {
+	return errors.New(fmt.Sprintf("orm:不支持的表达式 %v", expr))
+}
+
+func NewErrUnknownField(expr any) error {
+	return errors.New(fmt.Sprintf("orm:未知字段 %v", expr))
+}
