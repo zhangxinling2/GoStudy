15fe4bc6980ed18ea158313fdb9dc710cd089886
zhangxinling2 <2350486903@qq.com>
2023-03-23 14:16:42 +0800

    使用serverMux分发路由，测试POST和GET

---
+++ httpServer/httpBase/Client/main.go
@@ -0,0 +1,69 @@
+﻿package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	// ctx, close := context.WithTimeout(context.TODO(), 5*time.Second)
+	// defer close()
+	// go directGet()
+	// go httpGetWithContext(ctx)
+	postMethod()
+	time.Sleep(5 * time.Second)
+}
+func postMethod() {
+	r := strings.NewReader("http://localhost:8088/?name=xiaoqiang&&sex=男")
+	resp, err := http.Post("http://localhost:8088", "*/*", r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("resp: " + string(data))
+}
+func directGet() {
+	//Get
+	resp, err := http.Get("http://localhost:8088")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(data))
+}
+func httpGetWithContext(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	//生成请求
+	req, err := http.NewRequest("get", "http://localhost:8088", nil)
+	if err != nil {
+		log.Fatal("无法生成请求", err)
+	}
+	//将刚刚创建的上下文 ctx 传递给 HTTP 请求的 WithContext 方法，这样在进行请求时就会将上下文与请求关联起来
+	//一定要新赋值，因为源码中新建了一个req
+	req = req.WithContext(ctx)
+	//执行请求
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("无法发送请求", err)
+	}
+	//读取请求的结果
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("无法读取返回内容", err)
+	}
+	fmt.Println(string(data))
+}
---
+++ httpServer/httpBase/Server/main.go
@@ -0,0 +1,43 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/user", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello user"))
+	}))
+	mux.Handle("/rank", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello rank"))
+	}))
+	mux.Handle("/history/xiaoqiang", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello xiaoqiang"))
+	}))
+	mux.Handle("/history", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mq := r.URL.Query()
+		name := mq.Get("name")
+		w.Write([]byte(fmt.Sprintf("%s 的rank为", name)))
+	}))
+	http.ListenAndServe(":8080", mux)
+	// http.ListenAndServe(":8088", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	// time.Sleep(2 * time.Second)
+	// 	// w.Write([]byte("hello 你好"))
+
+	// 	//request读的时候要使用POST
+	// 	if r.Body == nil {
+	// 		w.Write([]byte("no body"))
+	// 		return
+	// 	}
+	// 	data, _ := ioutil.ReadAll(r.Body)
+	// 	defer r.Body.Close()
+	// 	encoded := base64.StdEncoding.EncodeToString(data)
+	// 	w.Write(append(data, []byte(encoded)...))
+
+	// 	// qp := r.URL.Query()
+	// 	// data, _ = json.Marshal(qp)
+	// 	// w.Write([]byte("hello 你好" + string(data)))
+	// }))
+}
